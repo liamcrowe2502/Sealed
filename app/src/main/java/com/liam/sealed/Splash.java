028a6bcde58365e7b90515738a468a97cf907b7b
Liam Crowe <20093470@mail.wit.ie>
2024-03-03 20:28:54 +0000

    Updating so users can switch and account validation for user in login.java file

--- app/src/main/java/com/liam/sealed/Splash.java
+++ app/src/main/java/com/liam/sealed/Splash.java
@@ -22,7 +22,7 @@ public class Splash extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(Splash.this, login.class);
+                Intent intent = new Intent(Splash.this, registration.class);
                 startActivity(intent);
                 finish();
             }
